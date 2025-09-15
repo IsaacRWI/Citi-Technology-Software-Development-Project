46b83ebc67047c3a93d151f8bc8cdb5b260d16af
Isaac <isaacngrwi@gmail.com>
2025-09-15 17:14:14 +0100

    Added class and method to fetch live stock price from yahoo finance

--- app/src/main/java/project/App.java
+++ app/src/main/java/project/App.java
@@ -1,15 +1,31 @@
 package project;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+
 import yahoofinance.YahooFinance;
 import yahoofinance.Stock;
 import java.util.Queue;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class App {
     private void retrieveData() {
         Queue<ArrayList<Object>> stockPriceQueue = new LinkedList<>();
-        
+        while (true) {
+            try {
+                // gets price of dow jones index
+                String ticker = "DJI";
+                Stock stock = YahooFinance.get(ticker);
+                BigDecimal price = stock.getQuote(true).getPrice();
+                //
+
+            }
+            catch(IOException e) {
+                System.out.println("Error: " + e);
+            }
+        }
+
     }
 }
