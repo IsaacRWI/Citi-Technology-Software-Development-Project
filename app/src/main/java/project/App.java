fc40a563448e33b71fc202ff95932b63d33bf8db
Isaac <isaacngrwi@gmail.com>
2025-09-15 15:40:46 +0100

    Added queue object to app - Imported relevant objects

--- app/src/main/java/project/App.java
+++ app/src/main/java/project/App.java
@@ -1,10 +1,15 @@
 package project;
 
-import java.io.IOExceptions;
+import java.io.IOException;
 import yahoofinance.YahooFinance;
 import yahoofinance.Stock;
 import java.util.Queue;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class App {
-    
+    private void retrieveData() {
+        Queue<ArrayList<Object>> stockPriceQueue = new LinkedList<>();
+        
+    }
 }
